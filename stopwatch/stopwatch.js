05e3d83b749ca83a9599ac7f4f1bbfd2b3b07c43
Marc Bah <marcbah123@gmail.com>
2022-05-25 22:39:02 +0200

    initial stopwatch

---
+++ stopwatch/stopwatch.js
@@ -0,0 +1,21 @@
+let interval
+let clock = 0
+function start() {
+    if (interval == undefined) {
+        interval = setInterval(clockincrease, 1000)
+        print()
+    }
+}
+function stop() {
+    clearInterval(interval)
+    interval = undefined
+    print()
+}
+function print() {
+    let stopwatch = document.getElementById("stopwatch")
+    stopwatch.textContent = clock
+}
+function clockincrease() {
+    clock = clock + 1
+    print()
+}
