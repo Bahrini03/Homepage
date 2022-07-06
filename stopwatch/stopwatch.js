d2b8add1831dbd6c584a997b6e0e9cfd9dbf6cd9
Marc Bah <marcbah123@gmail.com>
2022-07-06 11:57:32 +0200

    New time format

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -2,7 +2,7 @@ let interval
 let clock = 0
 function start() {
     if (interval == undefined) {
-        interval = setInterval(clockincrease, 1000)
+        interval = setInterval(clockincrease, 10)
         print()
     }
 }
@@ -13,15 +13,29 @@ function stop() {
 }
 function print() {
     let stopwatch = document.getElementById("stopwatch")
-    stopwatch.textContent = clock
+    stopwatch.textContent = formattime(clock)
 }
 function clockincrease() {
     clock = clock + 1
     print()
 }
-function reset () {
-    clock= 0
+function reset() {
+    clock = 0
     clearInterval(interval)
     interval = undefined
     print()
+}
+function formattime(milliseconds) {
+    let seconds = 0
+    let minutes = 0
+    let hours = 0
+
+    seconds = Math.floor(milliseconds / 100)
+    minutes = Math.floor(seconds / 60)
+    hours = Math.floor(minutes / 60)
+    return hours + ":" + formatnumber(minutes % 60) + ":" +formatnumber(seconds % 60)+ ":" +formatnumber(milliseconds % 100)
+}
+function formatnumber(num){
+    return  String(num).padStart(2,"0")
+
 }
